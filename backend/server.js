47ad41f59b208467bd3498f488d0aaf57b69a9b5
Gajjar-07 <nandgajjar2094@gmail.com>
2024-12-30 12:41:20 +0000

    server.js

--- backend/server.js
+++ backend/server.js
@@ -182,7 +182,7 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
   });
 });
-app.listen(3000, () => {
+app.listen(football-in-react.vercel.app, () => {
     console.log('Server is running on port 3000');
   
   });
